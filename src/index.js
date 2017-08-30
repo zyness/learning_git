bcc222ae60007ce899304c7daec1d3656f564c83
Vasili Abramov <vabramov@atrios.local>
2017-08-30 14:54:39 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker/>, document.querySelector('#main'));
